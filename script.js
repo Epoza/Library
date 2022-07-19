467769436ce8bd089da96291a72bd89c1f5dff4f
Epoza <jacobpittsley2@gmail.com>
2022-07-18 21:10:10 -0400

    Add modal

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const open = document.getElementById('open');
+const modalContainer = document.getElementById('modal-container')
+const close = document.getElementById('close');
+
+open.addEventListener('click', () => {
+    modalContainer.classList.add('show')
+})
+
+close.addEventListener('click', () => {
+    modalContainer.classList.remove('show')
+})
+
+
+
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
