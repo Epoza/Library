2df0aa519b78838f72d4dd0037b77ddeb34c9be5
Epoza <jacobpittsley2@gmail.com>
2022-07-27 20:54:04 -0400

    Add book objects into array

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const open = document.getElementById('open');
 const modalContainer = document.getElementById('modal-container')
 const close = document.getElementById('close');
+const cardHolder = document.getElementById('card-holder');
+const newCard = document.createElement('div');
 
 open.addEventListener('click', () => {
     modalContainer.classList.add('show')
@@ -11,17 +13,35 @@ close.addEventListener('click', () => {
 })
 
 
-
+let myLibrary = []
 
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function(){
-        return title + ' by ' + author + ', ' + pages + ' pages, ' + read
-    }
+    myLibrary.push(this)
+    // this.info = function(){
+    //     return title + ' by ' + author + ', ' + pages + ' pages, ' + read
+    // }
 }
 
-const animalFarm = new Book('Animal Farm', 'George Orwell', '289', 'not read yet')
-console.log(animalFarm.info())
+function addBookToLibrary() {
+    // add if statements to check form validity such as character length
+    console.log('ello')
+    title = document.getElementById('book-title').value;
+    author = document.getElementById('author').value;
+    pages = document.getElementById('pages').value;
+    read = document.getElementById('book-read').value;
+    new Book(title, author, pages, read) // takes form values and makes a new book
+    
+    console.log(myLibrary)
+}
+// const animalFarm = new Book('Animal Farm', 'George Orwell', '289', 'not read yet')
+// console.log(animalFarm.info())
+
+// document.getElementById('add').addEventListener('click', () => {
+//     if (1 === 1){
+//         addBookToLibrary()
+//     }
+// })
