a9bd0a301cd8871188eb13d68aa0d4b7a7986726
Epoza <jacobpittsley2@gmail.com>
2022-07-30 18:30:13 -0400

    Make new card when form is submitted

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const modalContainer = document.getElementById('modal-container')
 const close = document.getElementById('close');
 const cardHolder = document.getElementById('card-holder');
 const newCard = document.createElement('div');
+const form = document.getElementById('my-form');
+
+
 
 open.addEventListener('click', () => {
     modalContainer.classList.add('show')
@@ -27,21 +30,26 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-    // add if statements to check form validity such as character length
-    console.log('ello')
     title = document.getElementById('book-title').value;
     author = document.getElementById('author').value;
     pages = document.getElementById('pages').value;
     read = document.getElementById('book-read').value;
     new Book(title, author, pages, read) // takes form values and makes a new book
-    
+    createCard();
     console.log(myLibrary)
 }
-// const animalFarm = new Book('Animal Farm', 'George Orwell', '289', 'not read yet')
-// console.log(animalFarm.info())
 
-// document.getElementById('add').addEventListener('click', () => {
-//     if (1 === 1){
-//         addBookToLibrary()
-//     }
-// })
+function createCard(){
+    newCard.setAttribute('id', 'cards')
+    cardHolder.appendChild(newCard)
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    addBookToLibrary();
+    form.reset();
+});
+// function removeFromLibrary() {}
+
+// function editBook() {}
+
