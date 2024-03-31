373e902844839fe54e6d97936d1e8cec0ae69299
Epoza <86025026+Epoza@users.noreply.github.com>
2024-03-30 23:07:12 -0400

    add validation to other inputs

--- form_validation.js
+++ form_validation.js
@@ -1,13 +1,14 @@
 // Use for sending messages to user about forms
-console.log('test');
-const title = document.getElementById('title');
+const titleInput = document.getElementById('title');
 const titleError = document.getElementById('title-error')
+const authorInput = document.getElementById('author');
+const authorError = document.getElementById('author-error')
+const pagesInput = document.getElementById('pages');
+const pagesError = document.getElementById('pages-error')
 
-
-title.addEventListener("input", (event) => {
+titleInput.addEventListener("input", (event) => {
     // check if the form fields are valid.
-  
-    if (title.validity.valid) {
+    if (titleInput.validity.valid) {
       // if the field is valid, we remove the error message.
       titleError.textContent = ""; // Reset the content of the message
       titleError.className = "error"; // Reset the visual state of the message
@@ -17,11 +18,48 @@ title.addEventListener("input", (event) => {
     }
 });
 
+authorInput.addEventListener("input", (event) => {
+  // check if the form fields are valid.
+  if (authorInput.validity.valid) {
+    // if the field is valid, we remove the error message.
+    authorError.textContent = ""; // Reset the content of the message
+    authorError.className = "error"; // Reset the visual state of the message
+  } else {
+    // If there is still an error, show the correct error
+    showError();
+  }
+});
+
+pagesInput.addEventListener("input", (event) => {
+  // check if the form fields are valid.
+  if (pagesInput.validity.valid) {
+    // if the field is valid, we remove the error message.
+    pagesError.textContent = ""; // Reset the content of the message
+    pagesError.className = "error"; // Reset the visual state of the message
+  } else {
+    // If there is still an error, show the correct error
+    showError();
+  }
+});
+
 function showError() {
-    console.log('title error')
-    if (title.validity.valueMissing) {
-        // If the field is empty
-        titleError.textContent = 'You need to enter a book title';
+    if (titleInput.validity.valueMissing) {
+      // If the field is empty
+      titleError.textContent = 'You need to enter a book title';
+      titleError.className = 'error active';
     }
-    titleError.className = 'error active';
+    if(authorInput.validity.valueMissing){
+      // If the field is empty
+      authorError.textContent = 'You need to enter an author';
+      authorError.className = 'error active';
+    }
+    if(pagesInput.validity.valueMissing){
+      // If the field is empty
+      pagesError.textContent = 'You need to enter the amount of pages';
+      pagesError.className = 'error active';
+    } else if (pagesInput.validity.rangeOverflow){
+      // If the number > 9999
+      pagesError.textContent = 'Woah! Too many pages!';
+      pagesError.className = 'error active';
+    }
 }
